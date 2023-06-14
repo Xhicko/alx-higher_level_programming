208c9c355e8b23ea10a098e3583800dc69d19559
Xhicko <otagadaisaac@gmail.com>
2023-06-14 14:55:41 -0700

    More cahanges

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    for key in list(a_dictionary.keys()):
-        if a_dictionary[key] == value:
-            del a_dictionary[key]
-    return a_dictionary
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda i: i**2, x)), matrix))
