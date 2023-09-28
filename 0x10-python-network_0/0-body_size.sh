33a0cd605bebdfd3b56b9cd0c9d98f4ce341c924
Otagada Isaac <90391743+Xhicko@users.noreply.github.com>
2023-09-28 19:35:57 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Sends a request to that URL, and displays the size of the body of the response
-curl -s "${1}" | wc -c
+# curl -s "${1}" | wc -c
+#!/bin/bash
+# send a request to a URL with curl and display the size of the body of the response in bytes
+response=$(curl -s "$1")
+size=$(echo -n "$response" | wc --bytes)
+echo "Size of the response body in bytes: $size"
+
