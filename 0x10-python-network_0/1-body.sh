30e1704accce03c75a991c4fd537d38f4108dbd7
Otagada Isaac <90391743+Xhicko@users.noreply.github.com>
2023-09-28 19:39:19 +0100

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 # Sends a GET request to the URL, and displays the body of the response
-curl -s -L "${1}"
+#curl -s -L "${1}"
+url="$1"
+response=$(curl -s -L -w "%{http_code}" "$url")
+
+http_code=${response: -3}  # Extract the HTTP status code
+
+if [ "$http_code" == "200" ]; then
+    body=$(echo "$response" | sed '$d')  # Remove the last line (HTTP status code)
+    echo "$body"
+else
+    echo "HTTP status code: $http_code"
+fi
+
+
+
+
+
+
