738dbff9792f4b18c6a9063f020edabb7883cb9d
Otagada Isaac <90391743+Xhicko@users.noreply.github.com>
2023-09-29 15:44:44 +0100

    ALX 0x11-python-network_1 File

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" send request using requests method and get header id """
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" post email data into the url """
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    email = argv[2]
+
+    params = {'email': email}
+    result = requests.post(url, params)
+    print(result.text)
